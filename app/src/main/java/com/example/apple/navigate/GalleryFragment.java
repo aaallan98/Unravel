4f4a33247b7a9b46121834cf640e0323968c269a
SeoyeongPark <seoyeong0428@gmail.com>
2016-11-25 02:14:11 +0900

    Fix calendar layout

--- app/src/main/java/com/example/apple/navigate/GalleryFragment.java
+++ app/src/main/java/com/example/apple/navigate/GalleryFragment.java
@@ -23,7 +23,7 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
 
 }
