ae76932eff8a3e47a015a7b037f509ec21c5b160
Sarah-Juyoung <jusara1004@gmail.com>
2016-11-27 15:52:10 +0900

    Diary Fragment added

--- app/src/main/java/com/example/apple/navigate/DiaryFragment.java
+++ app/src/main/java/com/example/apple/navigate/DiaryFragment.java
@@ -2,16 +2,21 @@ package com.example.apple.navigate;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DiaryFragment extends Fragment {
+    //FloatingActionButton DateButton;
+   // int year_x, month_x,day_x;
+    //static final int Dialog_ID =0;
 
 
     public DiaryFragment() {
@@ -25,5 +30,9 @@ public class DiaryFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_diary, container, false);
     }
+
+    //public void showDialog(){
+       // DateButton = (FloatingActionButton)
+    //}
 
 }
